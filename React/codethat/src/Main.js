b12af63d9e2dcf250bca326d0cf80467c51c402e
Lilly1757 <seungji34@gmail.com>
2024-10-10 11:51:31 +0900

    Chore: 하위 페이지 분류

--- React/codethat/src/Main.js
+++ React/codethat/src/Main.js
@@ -1,28 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
 import HomePage from "./pages/HomePage";
-import CoursePage from './pages/CoursePage';
-import CourseListPage from './pages/CourseListPage';
-import WishlistPage from './pages/WishlistPage';
-import QuestionPage from './pages/QuestionPage';
-import QuestionListPage from './pages/QuestionListPage';
+import CoursePage from "./pages/CoursePage";
+import CourseListPage from "./pages/CourseListPage";
+import WishlistPage from "./pages/WishlistPage";
+import QuestionPage from "./pages/QuestionPage";
+import QuestionListPage from "./pages/QuestionListPage";
 
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route
-            path="courses/react-frontend-development"
-            element={<CoursePage />}
-          />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path="react-frontend-development" element={<CoursePage />} />
+          </Route>
+          <Route path="questions">
+            <Route index element={<QuestionListPage />} />
+            <Route path="616825" element={<QuestionPage />} />
+          </Route>
           <Route path="wishlist" element={<WishlistPage />} />
-          <Route path="questions" element={<QuestionListPage />} />
-          <Route path="questions/616825" element={<QuestionPage />} />
-        </Routes>
-      </App>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
